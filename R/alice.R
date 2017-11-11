2b23714fe28c5b8893f4bfd6310c4668945a5216
Andrew Shapiro <x-raizor@mail.ru>
2017-11-11 12:47:15 +0500

    Alice visualize update

--- R/alice.R
+++ R/alice.R
@@ -1,6 +1,7 @@
 library(lubridate)
+library(readr)
 
-df <- read_csv('/Users/andrew/reps/mlcourse_open/data/train/Alice_log.csv', 
+df <- read_csv('/Users/andrew/reps/mlcourse_open/data/alice/train/Alice_log.csv', 
                col_names = TRUE,
                col_types = list(
                  timestamp = col_datetime(format = ""),
@@ -12,6 +13,9 @@ df$mins <- hour(df$timestamp) * 60 + minute(df$timestamp)
 
 ggplot(df) + geom_col(aes(mins, 1))
 # how to draw X scale with hours and min
+
+ggplot(df) + geom_col(aes(hours, 1)) + scale_x_continuous(breaks = seq(0, 24, 1), 
+                                                          labels = as.character(seq(0, 24, 1)))
 
 min(df$timestamp)
 max(df$timestamp)
